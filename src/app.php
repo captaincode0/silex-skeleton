76b1c1bd1bc4d63ff96685768c4948ae553ede44
captaincode0 <developerdiego0@gmail.com>
2016-04-05 10:43:19 -0500

    Commit of skeleton working

--- src/app.php
+++ src/app.php
@@ -18,7 +18,7 @@
 			$this["production"]=false;
 			$this["https"]=false;
 			$this["asset.url"]=false;
-			$this["prefix-production.host"]="://www.productionvm.com/myapp/";
+			$this["prefix-production.host"]="://silexskeleton.herokuapp.com/";
 			$this["prefix.host"]="://www.myhost.com/myapp/";
 			$this["host"] = ($this["production"])?$this["prefix-production.host"]:$this["prefix.host"];
 			$this["host"] = ($this["https"])?"https".$this["host"]:"http".$this["host"];
@@ -26,20 +26,25 @@
 			//register framework service providers
 			$this->register(new UrlGeneratorServiceProvider());
 			$this->register(new HttpFragmentServiceProvider());
+			
 			$this->register(new TwigServiceProvider(), array(
 				"twig.path" => __DIR__."/../views"
 			));
+
 			$this->register(new MonologServiceProvider, array(
 				"monolog.logfile" => __DIR__."/../var/log/webapp-text-logger"
 			));
+
 			$this["twig"] = $this->extend("twig", function($twig, $app){
 				$twig->addFunction(new \Twig_SimpleFunction("asset", function($asset) use($app){
 					if($this["asset.url"])
-						return $app["host"]."assets".$app["request_stack"]->getMessageRequest()->getBasePath()."/".ltrim($asset, "/");
+						return $app["host"]."web/assets/".$app["request_stack"]->getMasterRequest()->getBasePath()."/".ltrim($asset, "/");
 					else
-						return "assets".$app["request_stack"]->getMessageRequest()->getBasePath()."/".ltrim($asset, "/");
+						return "web/assets/".ltrim($asset, "/");
  				}));
 				return $twig;
 			}); 
 		}
 	}
+
+	return new MyApplicationClass();
