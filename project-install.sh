b6e9ab7e205af1cb9df7c8d5ad9a6a8545a94a8d
captaincode0 <developerdiego0@gmail.com>
2016-11-01 01:53:45 -0600

    Changed project-install.sh

--- project-install.sh
+++ project-install.sh
@@ -8,7 +8,7 @@ composer install
 sudo cp vhosts.conf /etc/apache2/sites-available/silexskeleton.herokuapp.com.conf
 sudo a2ensite silexskeleton.herokuapp.com
 sudo service apache2 reload
-sudo sh -c 'echo 127.0.0.1		silexskeleton.herokuapp.com'
+sudo sh -c 'echo "127.0.0.1 	silexskeleton.herokuapp.com" >> /etc/hosts'
 sudo service networking restart
 sudo ping -c 4 silexskeleton.herokuapp.com
 firefox --new-window silexskeleton.herokuapp.com
