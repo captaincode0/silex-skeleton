57d14fc84f0027ddb4cc65d51e13d223069ef3f7
captaincode0 <developerdiego0@gmail.com>
2016-10-31 22:22:34 -0600

    Errors in configuration fixed

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-#delete the file before change permissions
+mkdir var
+mkdir var/log
+mkdir var/cache
+
 rm var/log/webapp-text-logger
 
 setfacl -R -m u:www-data:rwx var/log var/cache
@@ -12,5 +15,4 @@ setfacl -R -d -m u:`whoami`:rwx var/log var/cache
 setfacl -R -m mask:rwx var/log var/cache
 setfacl -R -d -m mask:rwx var/log var/cache
 
-#create the file with the right permissions
 touch var/log/webapp-text-logger
--- var/cache/readme.txt
+++
@@ -1,1 +0,0 @@
-All cache application data goes here
