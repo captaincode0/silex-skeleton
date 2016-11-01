f7feb4e56ebcdbbbf45a4aa65b61595940c072fd
captaincode0 <developerdiego0@gmail.com>
2016-11-01 01:14:54 -0600

    Changed fix-permissions script

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -4,13 +4,13 @@ mkdir var
 mkdir var/log
 mkdir var/cache
 
-setfacl -R -m u:www-data:rwx var/log var/cache
-setfacl -R -d -m u:www-data:rwx var/log var/cache
+sudo setfacl -R -m u:www-data:rwx var/log var/cache
+sudo setfacl -R -d -m u:www-data:rwx var/log var/cache
 
-setfacl -R -m u:`whoami`:rwx var/log var/cache
-setfacl -R -d -m u:`whoami`:rwx var/log var/cache
+sudo setfacl -R -m u:`whoami`:rwx var/log var/cache
+sudo setfacl -R -d -m u:`whoami`:rwx var/log var/cache
 
-setfacl -R -m mask:rwx var/log var/cache
-setfacl -R -d -m mask:rwx var/log var/cache
+sudo setfacl -R -m mask:rwx var/log var/cache
+sudo setfacl -R -d -m mask:rwx var/log var/cache
 
 touch var/log/webapp-text-logger
